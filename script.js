9ff168c13f2cd148cc3223a69dc4a48a828b4e0d
Azkieel <red.ezequiel@gmail.com>
2022-01-24 20:26:46 +0800

    write the rock paper scissor program

--- script.js
+++ script.js
@@ -1,78 +1,58 @@
-//prompt asking for user choice
+//run a prompt to ask user about their selection
+//the entered data is turn to lower case for functionality
 
-function userPlays() {
-    let userSelection = prompt("Make your selection: Rock, Paper, or Scissors?")
-    if (userSelection === null || userSelection === "" || userSelection === undefined) {
-        userPlays();
-    }
-    let choosenWeapon = userSelection.toLowerCase();
-    return choosenWeapon;
-}
+let userData = prompt("Rock, Paper, or Scissors? Choose one: ");
+let userSelection = userData.toLowerCase();
 
-let playerSelection = userPlays();
 
-//function that choose the computer's weapon
+//run a function for computer to choose its own selection
 
-const weapon = ['rock', 'paper', 'scissors'];
+const selection = ['rock', 'paper', 'scissors'];
 
 function computerPlays() {
-    random = Math.floor(Math.random() * weapon.length);
-    return weapon[random];
+    const random = Math.floor(Math.random() * selection.length);
+    return random[selection];
 }
 
 let computerSelection = computerPlays();
 
-//winner counter
+//both player and computer selection is match in a round
+//check who won based on the game rules
+//make tally on who wins
 
-let winner = 0;
-let loser = 0;
-
-//match player and computer selection
-//stores the winner
-
-function playRound(playerSelection,computerSelection) {
-    
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        winner++;
-        alert(`Your ${playerSelection} beat ${computerSelection}`);
-        return winner;
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        winner++;
-        alert(`Your ${playerSelection} beat ${computerSelection}`);
-        return winner;
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        winner++;
-        alert(`Your ${playerSelection} beat ${computerSelection}`);
-        return winner;
-    } else if (playerSelection === computerSelection) {
-        alert("It's a tie!")
-    }
-     else {
-        loser++;
-        alert(`Your ${playerSelection} lose to ${computerSelection}`);
-        return loser;
-    }
-}
+let playerTally = 0;
+let computerTally = 0;
 
 
-
-//function for a 5 round game
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection);
-    }
-    if (winner > loser) {
-        alert`Player won by ${winner} points`;
+function playRound(userSelection, computerSelection) {
+    if (userSelection === 'rock' && computerSelection === 'scissors') {
+        alert(`You win! ${userSelection} beats ${computerSelection}`)
+        playerTally++;
+        return playerTally;
+    } else if (userSelection === 'scissors' && computerSelection === 'paper') {
+        alert(`You win! ${userSelection} beats ${computerSelection}`)
+        playerTally++;
+        return playerTally;
+    } else if (userSelection === 'paper' && computerSelection === 'rock') {
+        alert(`You in! ${userSelection} beats ${computerSelection}`)
+        playerTally++;
+        return playerTally;
+    } else if (userSelection === computerSelection) {
+        alert(`It's a tie!`)
     } else {
-        alert`Computer wins by ${loser} points`;
+        alert(`You lose! Your ${userSelection} cannot beat ${computerSelection}`)
+        computerTally++;
+        return computerTally;
     }
 }
 
+console.log("player selection: ", userSelection);
+console.log("computer selection: ", computerSelection);
+console.log("player tally: ", playerTally);
+console.log("computer tally: ", computerTally);
 
-console.log("player", playerSelection);
-console.log("userplay function", userPlays());
-console.log("computer", computerSelection);
-console.log(playRound(playerSelection, computerSelection));
-console.log("winner count", winner);
-console.log("loser count", loser);
+console.log(playRound(userSelection, computerSelection));
+
+
+//create a function that run the match five times and announce the winner
+
